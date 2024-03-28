603ca5e722140555af0dd997a53d1ec4b6aa89f0
MohamedMohsenFahem <mn977850@gmail.com>
2024-03-29 01:47:35 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/egme_investigation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.egme_investigation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/Other Customers/other_customer.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OtherCustomers extends StatelessWidget {
+  const OtherCustomers({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
